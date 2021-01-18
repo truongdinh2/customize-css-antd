fcae0c749708dc96382b81e60f4a580e6b3efeeb
dinhtruong2 <dinhtruong2808@gmail.com>
2021-01-18 07:10:27 +0700

    jj

--- src/form.tsx
+++ src/form.tsx
@@ -1,80 +1,117 @@
 import { Form, Input, Button, Space, Row, Col, Divider, Typography } from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { FormValues } from './typesc/form';
+import { useEffect, useState } from 'react';
 
 const FormFile = () => {
+    const [dataForm, setDataForm] = useState({});
+    const [form] = Form.useForm()
     // const obj = {t:3,r:33,w:1}
     // console.log(obj['t'])
     // for( x in obj){
     //     console.log(x,obj['t'],obj[x])
     // }
-    const onFinish = (values: FormValues) => {
-        console.log('Received values of form:', values);
+    // useEffect(() => {
+    //     form.setFieldsValue({task1:dataForm});
+
+    //     console.log(dataForm)
+    //   });
+    useEffect(() => {
+        form.setFieldsValue( '');
+
+        // console.log()
+    });
+    const onFinish1 = async (values: any) => {
+        form.setFieldsValue( '');
+        console.log(values.user)
+        // if (!dataEdit) {
+        //     fetch("https://5fbb65b4c09c200016d406f6.mockapi.io/Project", {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         },
+        //         body: JSON.stringify(values.user),
+        //     }).then(() => {
+        //         success()
+        //         props.onChangeOpen()
+        //     });
+        // } else {
+        //     fetch(`https://5fbb65b4c09c200016d406f6.mockapi.io/Project/${dataEdit.id}`, {
+        //         method: "PUT",
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify(values.user),
+        //     }
+        //     ).then(() => {
+        //         props.onChangeOpen()
+        //         success();
+        //     })
+        // }
     };
 
+  
+    const onFinish = (values: FormValues) => {
+        setDataForm(values);
+        // form.set
+        console.log(form)
+        form.setFieldsValue('')
+        // form.resetFields([''])
+        console.log('hi')
+        console.log('Received values of form:', values);
+    };
+    const click = () => {
+        form.setFieldsValue({ task1: dataForm });
+        alert('hi')
+    }
     return (
-        // <Row gutter={15}>
-        //     <Col span={24}>
-        // <Space >
         <div style={{ width: '90%', margin: '10px auto' }}>
-
-            <Form name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
-                {/* <Space
+            <Form name="dynamic_form_nest_item" onFinish={onFinish}
+                form={form}
+                autoComplete="off">
+                <Space
                     direction="horizontal"
                     style={{ display: 'flex', justifyContent: "space-between", }}
                     align="center"
                     split={
                         <MinusCircleOutlined style={{ opacity: 0 }} />
-
                     }
                 >
                     <Form.Item
-                        // {...field}
-                        name={['users','first']}
-                        // fieldKey={[field.fieldKey, 'first']}
-                        rules={[{ required: true, message: 'Missing first name' }]}
+                        name={['task1', 'nameTask']}
+                        rules={[{ required: true, message: 'Missing this name' }]}
                     >
-                        <Input placeholder="First Name" />
+                        <Input placeholder="Named task" />
                     </Form.Item>
                     <Form.Item
-                        // {...field}
-                        // name={[field.name, 'last']}
-                        // fieldKey={[field.fieldKey, 'last']}
-                        rules={[{ required: true, message: 'Missing last name' }]}
+
+                        name={['task1', 'contentTask']}
+                        rules={[{ required: true, message: 'Missing this name' }]}
                     >
-                        <Input placeholder="Last Name" />
+                        <Input placeholder="Content task" />
                     </Form.Item>
-                </Space> */}
-                <Form.List name="users">
+                </Space>
+                <Form.List name={['task1', 'users']}>
                     {(fields, { add, remove }) => (
                         <>
-                            {console.log(fields)}
                             {fields.map(field => (
                                 <Space key={field.key}
-                                    style={{ display: 'flex' }}
+                                    style={{ display: 'flex', flexDirection: 'row-reverse' }}
                                     align="baseline"
                                 >
                                     <Form.Item
                                         {...field}
-                                        name={[field.name, 'first']}
+                                        name={[field.name, 'contentTask']}
                                         fieldKey={[field.fieldKey, 'first']}
-                                        rules={[{ required: true, message: 'Missing first name' }]}
+                                        rules={[{ required: true, message: 'Missing this field' }]}
                                     >
-                                        <Input placeholder="First Name" />
+                                        <Input placeholder="Content task" />
                                     </Form.Item>
                                     <MinusCircleOutlined onClick={() => remove(field.name)} />
-                                    {/* <Form.Item
-                                        {...field}
-                                        name={[field.name, 'last']}
-                                        fieldKey={[field.fieldKey, 'last']}
-                                        rules={[{ required: true, message: 'Missing last name' }]}
-                                    >
-                                        <Input placeholder="Last Name" />
-                                    </Form.Item> */}
                                 </Space>
                             ))}
                             <Form.Item>
-                                <Button type="dashed" onClick={() => add(0)} block icon={<PlusOutlined />}>
+                                <Button type="dashed" onClick={() => { add() }} block icon={<PlusOutlined />}>
                                     Add field
                         </Button>
                             </Form.Item>
@@ -92,6 +129,36 @@ const FormFile = () => {
                 <Typography.Link>Link</Typography.Link>
                 <Typography.Link>Link</Typography.Link>
             </Space>
+            <button onClick={click}>hi</button>
+
+
+
+            <Form name="nest-messages" onFinish={onFinish1}
+                form={form}>
+                <Form.Item name={['user', 'Name']} label="Name" rules={[{ required: true }]}>
+                    <Input
+                    />
+                </Form.Item>
+
+                <Form.Item name={['user', 'Date_begin']} label="Date begin" rules={[
+                    { required: true }]}>
+                    <Input type="date" />
+                </Form.Item>
+                <Form.Item name={['user', 'Time_expected']}
+                    label="Time expected" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'NumberPepole']} label="Leader"
+                    rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item >
+                    <Button type="primary" htmlType="submit" className="btn"
+                    >
+                        Submit
+        </Button>
+                </Form.Item>
+            </Form>
         </div>
 
     )
